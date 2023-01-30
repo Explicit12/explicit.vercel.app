39b0e3fd6ab6a63ea9ab9b52d2ff501ee5c916c4
Explicit12 <explicit1723@gmail.com>
2023-01-30 19:26:19 +0200

    added meta tags

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,29 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      htmlAttrs: {
+        lang: "en",
+      },
+      title: "Dmytro Holdobin",
+      meta: [
+        { charset: "UTF-8" },
+        { name: "viewport", content: "width=device-width, initial-scale=1.0" },
+        {
+          name: "description",
+          content: "Personal site of front end developer Dmytro Holdobin",
+        },
+        { name: "theme-color", content: "#0C0C0D" },
+        { name: "ICBM", content: "49.04791247867182, 31.0413272122303" },
+        {
+          name: "geo.position",
+          content: "49.04791247867182; 31.0413272122303",
+        },
+        {},
+      ],
+    },
+  },
+
   modules: [
     "@nuxtjs/google-fonts",
     "@nuxtjs/apollo",
