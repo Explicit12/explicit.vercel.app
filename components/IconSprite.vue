7c4af2ba2afd7a15337656575b2bbcd88055767c
Explicit12 <explicit1723@gmail.com>
2023-02-04 01:37:53 +0200

    created icon component

---
+++ components/IconSprite.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+  withDefaults(
+    defineProps<{ iconName: string; height?: number; width?: number }>(),
+    { height: 24, width: 24 },
+  );
+</script>
+
+<template>
+  <svg class="icon">
+    <use :xlink:href="`/svg/icon-sprites.svg#${iconName}`" />
+  </svg>
+</template>
+
+<style scoped>
+  .icon {
+    width: v-bind(String(width) + "px");
+    height: v-bind(String(height) + "px");
+  }
+</style>
