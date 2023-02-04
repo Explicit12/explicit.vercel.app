c21393deb0285c83d8473fd75c3760f26e11dffe
Explicit12 <explicit1723@gmail.com>
2023-02-04 12:14:09 +0200

    added header component

---
+++ components/TheHeader.vue
@@ -0,0 +1,83 @@
+<template>
+  <header class="header">
+    <nav class="header__navigation container">
+      <CustomLink to="/">
+        <IconSprite icon-name="HyperLink" />
+        Home
+      </CustomLink>
+
+      <AppButton
+        class="header__navigation__menu-button"
+        type="secondary"
+        aria-label="Open mobile menu"
+      >
+        <IconSprite icon-name="JustifyAlignment" />
+      </AppButton>
+      <ul class="header__navigation__anchor-links">
+        <li>
+          <CustomLink to="/">
+            <IconSprite icon-name="HyperLink" />
+            Skills
+          </CustomLink>
+        </li>
+        <li>
+          <CustomLink to="/">
+            <IconSprite icon-name="HyperLink" />
+            Portfolio
+          </CustomLink>
+        </li>
+        <li>
+          <CustomLink to="/">
+            <IconSprite icon-name="HyperLink" />
+            About me
+          </CustomLink>
+        </li>
+        <li>
+          <CustomLink to="/">
+            <IconSprite icon-name="HyperLink" />
+            Contacts
+          </CustomLink>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<style scoped>
+  .header {
+    padding: 8px 0;
+    position: sticky;
+    top: 0;
+
+    background: rgba(12, 12, 13, 0.85);
+    backdrop-filter: blur(9px);
+  }
+
+  .header__navigation {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  .header__navigation__anchor-links {
+    display: none;
+  }
+
+  @media screen and (min-width: 1024px) {
+    .header__navigation__menu-button {
+      display: none;
+    }
+
+    .header__navigation__anchor-links {
+      list-style-type: none;
+
+      padding: 0;
+      width: 100%;
+      max-width: 608px;
+
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+    }
+  }
+</style>
