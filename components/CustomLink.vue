0fa410624b67affd0e9380e54c66011652f06c5d
Explicit12 <explicit1723@gmail.com>
2023-02-04 12:13:27 +0200

    added custom link component

---
+++ components/CustomLink.vue
@@ -0,0 +1,22 @@
+<template>
+  <NuxtLink class="link text">
+    <slot />
+  </NuxtLink>
+</template>
+
+<style scoped>
+  .link {
+    display: flex;
+    align-items: center;
+    gap: 8px;
+
+    text-decoration: none;
+    transition: color 250ms;
+    color: var(--primary-white);
+  }
+
+  .link:hover {
+    transition: color 250ms;
+    color: var(--accent-blue);
+  }
+</style>
