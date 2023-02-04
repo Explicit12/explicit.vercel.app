89daf9fa524cb0740362b0d9e1e2e39eda072539
Explicit12 <explicit1723@gmail.com>
2023-02-04 12:13:51 +0200

    added app button component

---
+++ components/AppButton.vue
@@ -0,0 +1,83 @@
+<script setup lang="ts">
+  import { computed } from "vue";
+
+  const props = defineProps<{
+    type: "primary" | "secondary";
+    block?: boolean;
+  }>();
+
+  const isPrimary = computed(() => props.type === "primary");
+</script>
+
+<template>
+  <button
+    type="button"
+    class="button"
+    :class="{
+      button_block: block,
+      button_primary: isPrimary,
+      button_secondary: !isPrimary,
+    }"
+  >
+    <slot />
+  </button>
+</template>
+
+<style scoped>
+  .button {
+    border: none;
+    outline: none;
+    background: none;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 8px;
+
+    padding: 18px 24px;
+    border-radius: 8px;
+
+    font-family: "Inter";
+    font-style: normal;
+    font-weight: 700;
+    font-size: 24px;
+    text-transform: uppercase;
+    color: var(--primary-white);
+  }
+
+  .button:hover:not(:disabled) {
+    cursor: pointer;
+  }
+
+  .button:focus {
+    outline: var(--accent-light) solid 2px;
+  }
+
+  .button_block {
+    width: 100%;
+  }
+  .button_primary {
+    transition: box-shadow 250ms;
+    background-color: var(--accent);
+  }
+
+  .button_primary:hover,
+  .button_primary:focus {
+    transition: box-shadow 250ms;
+    box-shadow: 0px 8px 19px rgba(255, 0, 98, 0.32);
+    outline: var(--accent-light) solid 2px;
+  }
+
+  .button_primary:disabled {
+    background-color: var(--secondary-black);
+    color: var(--secondary-white);
+  }
+
+  .button_secondary {
+    color: var(--primary-white);
+  }
+
+  .button_secondary:disabled {
+    color: var(--secondary-white);
+  }
+</style>
