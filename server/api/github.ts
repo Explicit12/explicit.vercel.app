c4eae0caad2ac2da0d64165746967afa08a001b4
Explicit12 <explicit1723@gmail.com>
2023-02-12 14:24:24 +0200

    moved client to handler function

--- server/api/github.ts
+++ server/api/github.ts
@@ -3,20 +3,20 @@ import GithubQuery from "~/server/queries/github";
 
 import type GitHubInfo from "~/types/GitHubInfo";
 
-const client = new ApolloClient({
-  uri: "https://api.github.com/graphql",
-  cache: new InMemoryCache(),
-  headers: {
-    Authorization: "github token here",
-  },
-});
-
 export default defineEventHandler(async (event) => {
   setResponseHeaders(event, {
     "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Methods": "GET",
     "Access-Control-Allow-Credentials": "true",
     "Cross-Origin-Resource-Policy": "same-origin",
+  });
+
+  const client = new ApolloClient({
+    uri: "https://api.github.com/graphql",
+    cache: new InMemoryCache(),
+    headers: {
+      Authorization: "github token here",
+    },
   });
 
   const { data } = await client.query({
