bb917770368451d67e44dde05eb00d403b953711
Explicit12 <explicit1723@gmail.com>
2023-02-12 09:44:30 +0200

    moved erorr page styles to separate component becouse of styles were not working in production

---
+++ components/TheError.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+  defineProps<{ errorCode: number }>();
+  defineEmits<{ (e: "buttonClick"): void }>();
+</script>
+
+<template>
+  <div class="error">
+    <section class="error__note">
+      <h1 class="text-h1 error__note__headline">{{ errorCode }} Error</h1>
+      <p class="text error__note__text">
+        Sorry for that. It may be an internal server error or you're trying to
+        reach nonexistent page.
+      </p>
+      <AppButton type="primary" @click="$emit('buttonClick')">
+        To main page
+      </AppButton>
+    </section>
+    <TheFooter />
+  </div>
+</template>
+
+<style scoped>
+  .error {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    height: 100vh;
+  }
+
+  .error__note {
+    background-color: var(--primary-black);
+    height: 100%;
+    color: var(--primary-white);
+
+    text-align: center;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+  }
+
+  .error__note__headline {
+    padding-bottom: 24px;
+  }
+
+  .error__note__text {
+    padding-bottom: 16px;
+    max-width: 826px;
+  }
+</style>
--- error.vue
+++ error.vue
@@ -13,47 +13,5 @@
 </script>
 
 <template>
-  <div class="error">
-    <section class="error__note">
-      <h1 class="text-h1 error__note__headline">
-        {{ error.statusCode }} Error
-      </h1>
-      <p class="text error__note__text">
-        Sorry for that. It may be an internal server error or you're trying to
-        reach nonexistent page.
-      </p>
-      <AppButton type="primary" @click="handleError"> To main page </AppButton>
-    </section>
-    <TheFooter />
-  </div>
+  <TheError :error-code="error.statusCode" @button-click="handleError" />
 </template>
-
-<style scoped>
-  .error {
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    height: 100vh;
-  }
-
-  .error__note {
-    background-color: var(--primary-black);
-    height: 100%;
-    color: var(--primary-white);
-
-    text-align: center;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-  }
-
-  .error__note__headline {
-    padding-bottom: 24px;
-  }
-
-  .error__note__text {
-    padding-bottom: 16px;
-    max-width: 826px;
-  }
-</style>
